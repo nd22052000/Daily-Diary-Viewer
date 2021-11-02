dfb1cf645b8a8fe47d18faa7e9b2bc9dee1befe5
nd22052000 <87229442+nd22052000@users.noreply.github.com>
2021-11-02 15:08:23 +0530

    added finalData.js https://codesandbox.io/s/sparkling-sea-hxeum?file=/src/Components/FinalData.js

---
+++ src/Components/FinalData.js
@@ -0,0 +1,20 @@
+import React from "./react";
+
+
+function display() {
+  var data=[
+    {
+      spraying:{Type:arguments,Chemi},
+      fertilization:{},
+      farmWork:{},
+      soilWork:{},
+      maintenance:{}
+    }
+  ]
+  return(
+
+  )
+}
+
+
+export default display;
